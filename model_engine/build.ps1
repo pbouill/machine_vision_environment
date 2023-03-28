58f8f0b8df49b25b13e26b5ec5f47a0003e33b62
hatch-pbouillon <paul.bouillon@hatch.com>
2023-03-28 18:18:34 -0400

    updated ps for invoke-webrequest args...

--- model_engine/build.ps1
+++ model_engine/build.ps1
@@ -1,4 +1,4 @@
-$REQ_FILE_VER = curl -s "https://api.github.com/repos/pbouill/model_engine/commits?path=requirements.txt&page=1&per_page=1" | jq -r '.[0].sha'      
+$REQ_FILE_VER = (curl -URI "https://api.github.com/repos/pbouill/model_engine/commits?path=requirements.txt&page=1&per_page=1").content | jq -r '.[0].sha'      
 $REPO_VER = (git ls-remote https://github.com/pbouill/model_engine.git).split()[0]
 echo "Preparing to build the image with arguments..."
 echo "Last commit of requirements.txt: $REQ_FILE_VER"
